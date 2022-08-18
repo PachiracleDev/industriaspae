7e1eda810f1a7de3522e57fe8045093d986b4cae
Pachiracle <pachiracledev@gmail.com>
2022-08-18 09:59:20 -0500

    version-1.1.1.1

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
+
 import { createPool } from "mysql2/promise";
 
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+
 const pool = createPool({
-  host: "containers-us-west-23.railway.app",
-  user: "root",
-  password: "kSrna8F1uVG3WhTzEJjB",
-  database: "railway",
-  port: 7014,
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
